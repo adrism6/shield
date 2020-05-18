64cac68fac565d9184a26e206c9946e1943d5107
adrism6 <adrian6101995@gmail.com>
2020-05-18 08:30:29 +0100

    add rpoject files

--- metahumans/models.py
+++ metahumans/models.py
@@ -2,26 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Power(models.Model):
-
-    class Meta:
-        verbose_name = "Poder"
-        verbose_name_plural = "Poderes"
-
-    name = models.CharField(max_length=32)
-    description = models.CharField(max_length=300)
-    level = models.IntegerField(default=50)
-
-    def __str__(self):
-        return f'{self.name}: {self.description}'
-
-COUNTRIES = [
-    ('US', 'United states'),
-    ('ES', 'Spain'),
-    ('UK', 'United Kingdom'),
-    ('OT', "Others"),
-    ]
-
 class Team(models.Model):
 
     class Meta:
@@ -37,27 +17,52 @@ class Team(models.Model):
         return self.name
 
 
+class Power(models.Model):
+
+    class Meta:
+        verbose_name = "Poder"
+        verbose_name_plural = "Poderes"
+        ordering = ['name']
+
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=300)
+    level = models.IntegerField(default=50)
+
+    def __str__(self):
+        return self.name
+
+
+COUNTRIES = [
+    ('US', 'United states'),
+    ('ES', 'Spain'),
+    ('UK', 'United Kingdom'),
+    ('PL', 'Polonia'),
+    ('WK', 'Wakanda'),
+    ('LV', 'Lavteria'),
+    ('RU', 'Rusia'),
+    ('OT', "Others"),
+    ]
+
+
 class MetaHuman(models.Model):
 
     class Meta:
         verbose_name = "Metahumano"
-        verbose_name_plural = "Metahumano"
+        verbose_name_plural = "Metahumanos"
 
     name = models.CharField(max_length=42)
     country = models.CharField(max_length=2, choices=COUNTRIES)
     level = models.IntegerField(default=10)
     active = models.BooleanField(default=True)
     powers = models.ManyToManyField(Power)
-    ally = models.BooleanField(default=True)
     last_update = models.DateTimeField(auto_now=True)
     team = models.ForeignKey(
-            Team,
-            on_delete=models.PROTECT,
-            blank=True,
-            null=True,
-            default=None,
-        )
+        Team,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        default=None,
+    )
 
     def __str__(self):
         return self.name
-
